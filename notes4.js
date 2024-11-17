57126e255a492a034a503d43365af92ec8f96e77
Phionanamugga <phionanamugga15@gmail.com>
2024-11-17 08:22:13 +0300

    adds javascript for the new notes page

--- notes4.js
+++ notes4.js
@@ -21,5 +21,6 @@ redoBtn.addEventListener("click", ()=>{
 
 // Save content to local storage
 saveBtn.addEventListener("click",()=>{
-    
+    localStorage.setItem("noteContent", noteArea.innerHTML);
+
 })
