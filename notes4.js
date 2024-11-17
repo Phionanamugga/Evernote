63046ca84b8252a64b1694b71ae1835833ee0ac0
Phionanamugga <phionanamugga15@gmail.com>
2024-11-17 08:07:13 +0300

    adds javascript for the new notes page

--- notes4.js
+++ notes4.js
@@ -1,3 +1,9 @@
 const noteArea = document.getElementById("noteArea");
 const redoBtn = document.getElementById("redoBtn");
 const shareBtn = document.getElementById("shareBtn")
+const saveBtn = document.getElementById("saveBtn");
+
+// For undo/redo functionality
+let undoStack = [];
+let redoStack = [];
+
